e01ebadc435b0dd7f3d9ced1f8c92339bb417d4e
moremoneymod <84ivan48@gmail.com>
2024-10-02 19:44:11 +0300

    добавлена функция для получения координат столбцов групп, теперь парсер добавляет пустые пары

--- parser.py
+++ parser.py
@@ -3,7 +3,7 @@ import os.path
 from xls2xlsx import XLS2XLSX
 from openpyxl import load_workbook
 from openpyxl.worksheet.worksheet import Worksheet
-from typing import List
+from typing import List, Dict
 import re
 
 
@@ -20,15 +20,19 @@ class SheduleParser:
         self._workbook = load_workbook(self._document_path)
         self._worksheet = self._workbook["2022-2023"]
 
-    def read_sheet(self) -> None:
+    def read_sheet(self) -> Dict:
         lesson_time = None
         day = None
         lessons = {}
+        group_column_coordinates = self.get_groups_coordinates()
         for row in self._worksheet.iter_rows(min_row=7):
             for cell in row:
-                if cell.value is None:
+                if cell.value is None and cell.column in group_column_coordinates:
+                    cell_value = "Нет пар"
+                elif cell.value is None:
                     continue
-                cell_value = str(cell.value)
+                else:
+                    cell_value = str(cell.value)
                 if cell_value == '1' or cell_value == '3' or cell_value == '5' or cell_value == '7':
                     lesson_time = cell_value + f'-{int(cell_value) + 1} урок'
                     if lesson_time not in lessons[day]:
@@ -44,6 +48,10 @@ class SheduleParser:
                         subject = re.sub(" +", " ", cell_value.strip())
                         lessons[day][lesson_time].append(subject)
         print(lessons)
+        return lessons
+
+    def get_shedule(self) -> None:
+        groups = self.get_group_columns()
 
     def get_group_columns(self, group_row_index=6) -> List:
         groups = []
@@ -59,6 +67,19 @@ class SheduleParser:
                     else:
                         cell_value = cell_value.replace(' ', '')
                         groups.append(cell_value)
-
         print(groups)
         return groups
+
+    def get_groups_coordinates(self, group_row_index=6) -> List:
+        group_coordinates = []
+        for row in self._worksheet.iter_rows(min_row=group_row_index, max_row=group_row_index):
+            for cell in row:
+                if cell.value is None:
+                    continue
+                cell_value = cell.value
+                if cell_value.count('-'):
+                    group_coordinates.append(cell.column)
+                    print(cell.column)
+        return group_coordinates
+
+
