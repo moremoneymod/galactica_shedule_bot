8eff802dfa6dbfe1bf7f02fc809abea86b86966f
moremoneymod <84ivan48@gmail.com>
2024-10-04 17:45:20 +0300

    некоторые улучшения парсера + он полностью функционирует, но код все так же сырой

--- parser.py
+++ parser.py
@@ -3,8 +3,9 @@ import os.path
 from xls2xlsx import XLS2XLSX
 from openpyxl import load_workbook
 from openpyxl.worksheet.worksheet import Worksheet
-from typing import List, Dict
+from typing import List, Dict, Any
 import re
+from utils import json_utils
 
 
 class SheduleParser:
@@ -20,45 +21,11 @@ class SheduleParser:
         self._workbook = load_workbook(self._document_path)
         self._worksheet = self._workbook["2022-2023"]
 
-    def read_sheet(self) -> Dict:
-        lesson_time = None
-        day = None
-        lessons = {}
-        group_column_coordinates = self.get_groups_coordinates()
-        for row in self._worksheet.iter_rows(min_row=7):
-            for cell in row:
-                if cell.value is None:
-                    if cell.column in group_column_coordinates:
-                        cell_value = "Нет пары"
-                    else:
-                        continue
-                else:
-                    cell_value = str(cell.value)
-                if cell_value == '1' or cell_value == '3' or cell_value == '5' or cell_value == '7':
-                    lesson_time = cell_value + f'-{int(cell_value) + 1} урок'
-                    if lesson_time not in lessons[day]:
-                        lessons[day][lesson_time] = []
-                if cell_value.strip().lower() in ["понедельник", "вторник", "среда", "четверг", "пятница"]:
-                    day = cell_value.strip()
-                    if day not in lessons:
-                        lessons[day] = {}
-                else:
-                    print(cell_value.strip(), day, lesson_time)
-                    print(lessons)
-                    if len(cell_value) > 1:
-                        subject = re.sub(" +", " ", cell_value.strip())
-                        lessons[day][lesson_time].append(subject)
-        print(lessons)
-        return lessons
-
-    def get_shedule(self) -> None:
-        groups = self.get_group_columns()
-
-    def get_group_columns(self, group_row_index=6) -> List:
-        groups = []
+    def get_groups(self, group_row_index: int = 6) -> List:
+        groups: List = []
         for row in self._worksheet.iter_rows(min_row=group_row_index, max_row=group_row_index):
             for cell in row:
-                cell_value = str(cell.value)
+                cell_value: str = str(cell.value)
                 if cell_value.count('-') >= 1:
                     if cell_value.count('-') > 1:
                         if cell_value.count(',') == 0:
@@ -71,29 +38,65 @@ class SheduleParser:
         print(groups)
         return groups
 
-    def get_groups_coordinates(self, group_row_index=6) -> List:
-        group_coordinates = []
+    def get_group_coordinates(self, group_row_index: int = 6) -> List:
+        study_group_coordinates: List = []
         for row in self._worksheet.iter_rows(min_row=group_row_index, max_row=group_row_index):
             for cell in row:
-                if cell.value is None:
+                if cell.value is None or cell.value.count('-') == 0:
                     continue
-                cell_value = cell.value
-                if cell_value.count('-'):
-                    group_coordinates.append(cell.column)
-                    print(cell.column)
-        return group_coordinates
+                else:
+                    study_group_coordinates.append(cell.column)
+        return study_group_coordinates
 
-    def get_shedule_for_each_group(self) -> Dict:
-        groups = self.get_group_columns()
-        raw_shedule = self.read_sheet()
-        shedule = {}
+    def read_sheet(self) -> Dict:
+        lesson_time: str | None = None
+        day: str | None = None
+        lessons: Dict = {}
+        group_column_coordinates: List = self.get_group_coordinates()
+
+        study_days: List = ["понедельник", "вторник", "среда", "четверг", "пятница"]
+        lesson_numbers: List = ['1', '3', '5', '7']
+
+        for row in self._worksheet.iter_rows(min_row=7):
+            for cell in row:
+                cell_value: Any = cell.value
+                if cell_value is None:
+                    if cell.column in group_column_coordinates:
+                        cell_value = "Нет пары"
+                    else:
+                        continue
+                else:
+                    cell_value = str(cell.value)
+                if cell_value in lesson_numbers:
+                    lesson_time = cell_value + f'-{int(cell_value) + 1} урок'
+                    if lesson_time not in lessons[day]:
+                        lessons[day][lesson_time] = []
+                elif cell_value.strip().lower() in study_days:
+                    day = cell_value.strip()
+                    if day not in lessons:
+                        lessons[day] = {}
+                else:
+                    if len(cell_value) > 1:
+                        subject: str = re.sub(" +", " ", cell_value.strip())
+                        lessons[day][lesson_time].append(subject)
+        print(lessons)
+        return lessons
+
+    def get_schedule_for_each_group(self) -> Dict:
+        groups: List = self.get_groups()
+        raw_schedule: Dict = self.read_sheet()
+        schedule: Dict = {}
         for group_index in range(len(groups)):
-            lesson_index = group_index
-            shedule[groups[group_index]] = {}
-            for day in raw_shedule:
-                shedule[groups[group_index]][day] = {}
-                for time in raw_shedule[day]:
-                    print(day, time, raw_shedule[day][time][lesson_index], groups[group_index])
-                    shedule[groups[group_index]][day][time] = raw_shedule[day][time][lesson_index]
-        print(shedule)
-        return shedule
+            lesson_index: int = group_index
+            schedule[groups[group_index]] = {}
+            for day in raw_schedule:
+                schedule[groups[group_index]][day] = {}
+                for time in raw_schedule[day]:
+                    schedule[groups[group_index]][day][time] = raw_schedule[day][time][lesson_index]
+        print(schedule)
+        return schedule
+
+    def get_json_schedule(self, schedule: Dict) -> None:
+        json_schedule = json_utils.json_converter(schedule)
+        print(json_schedule)
+        json_utils.save_json(json_schedule)
